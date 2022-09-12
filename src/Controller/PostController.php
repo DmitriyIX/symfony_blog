77e86a5b2db403cc0408783b355d0b715a81e74c
Дмитрий Ежков <dmitrii.ezhkov@simbirsoft.com>
2022-09-12 18:56:44 +0400

    Создание главной страницы

---
+++ src/Controller/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/')]
+    public function indexPage(): Response
+    {
+        return new Response (
+            '<div>index page</div>'
+        );
+    }
+
+    /**
+     * @Route("/post", name="app_post")
+     */
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+}
